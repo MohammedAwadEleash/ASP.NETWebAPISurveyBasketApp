4e0b8492b95917c4a47a11a387b4e1585cf333cb
Mohammed Awad <mohammed.awad1700@gmail.com>
2025-02-09 05:56:27 +0200

    Add CRUD Operation Polls Controller

---
+++ SurveyBasket/Controllers/PollsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace SurveyBasket.Controllers
+
+{
+    [Route("api/[controller]")]   ///api/Pools
+    [ApiController]
+    public class PollsController(IPollService pollService) : ControllerBase
+    {
+        private readonly IPollService _pollService = pollService;
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()  //api/polls
+
+        {
+
+            return Ok(_pollService.GetAll());
+        }
+
+        [HttpGet("{id}")]   //api/polls/1
+
+        public IActionResult Get(int id)  
+        {
+            var poll = _pollService.Get(id);
+            if (poll is null)
+                return NotFound();
+
+            return Ok(poll);
+        }
+        [HttpPost("")]
+        public IActionResult Add(Poll request)
+
+        {
+
+            var newPoll = _pollService.Add(request);
+            return CreatedAtAction(nameof(Get), new {id= newPoll.Id }, newPoll);
+                
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id ,Poll request)
+
+        {
+
+            var isUpdated = _pollService.Update(id,request);
+            if (!isUpdated)
+                return NotFound();
+            return NoContent();
+
+
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+
+        {
+
+            var isDeleted = _pollService.Delete(id);
+            if (!isDeleted)
+                return NotFound();
+            return NoContent();
+
+
+        }
+    }
+}
---
+++ SurveyBasket/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using SurveyBasket.Models;
+global using SurveyBasket.Services;
